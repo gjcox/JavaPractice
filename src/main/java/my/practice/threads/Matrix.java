3dc6091402db7b632d0e9faba4074d6c1e85b56f
Gabriel <gabrielcoxomega@gmail.com>
2024-03-29 12:28:31 +0000

    feat: implement Matrix constructor and getters

--- src/main/java/my/practice/threads/Matrix.java
+++ src/main/java/my/practice/threads/Matrix.java
@@ -1,18 +1,47 @@
 package my.practice.threads;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Matrix {
-    public Matrix(int[][] arr) {
-        // TODO
+    private static final int INT_PADDING = 2;
+
+    private final int height, width;
+    private final int[][] arr;
+
+    public Matrix(int height, int width) {
+        this.height = height;
+        this.width = width;
+        this.arr = new int[height][width];
     }
 
-    public int getWidth() {
-        // TODO
-        return 0;
+    public Matrix(int[][] arr) {
+        this.arr = arr;
+        this.height = arr.length;
+        this.width = arr[0].length;
+        for (int[] row : arr) {
+            if (row.length != this.width) {
+                throw new IllegalArgumentException("Matrix rows must all the same length");
+            }
+        }
     }
 
     public int getHeight() {
-        // TODO
-        return 0;
+        return height;
     }
 
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int[] row : arr) {
+            builder.append(IntStream.of(row)
+                    .mapToObj(i -> String.format("%" + INT_PADDING + "d", i))
+                    .collect(Collectors.joining(" ", "| ", " |\n")));
+        }
+        return builder.toString();
+    }
 }
