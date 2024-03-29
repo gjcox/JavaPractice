b16f12dd2f1274b8f35b00fa1b5e163fb180daff
Gabriel <gabrielcoxomega@gmail.com>
2024-03-29 11:16:09 +0000

    feat: implement the parity thread exercise

--- src/main/java/my/practice/Threads.java
+++ src/main/java/my/practice/Threads.java
@@ -16,8 +16,37 @@ public class Threads {
         }
     }
 
+    /**
+     * Prints all of the odd or even integers from 0 up to a specified limit.
+     */
+    public static class ParityRunnable implements Runnable {
+        private int max;
+        private boolean even;
+
+        /**
+         * 
+         * @param max  the value at which to stop printing (inclusive)
+         * @param even true to print even numbers; false to print odd numbers
+         */
+        public ParityRunnable(int max, boolean even) {
+            this.max = max;
+            this.even = even;
+        }
+
+        @Override
+        public void run() {
+            for (int i = even ? 0 : 1; i <= max; i += 2)
+                System.out.println((even ? "even" : "odd") + ": " + i);
+        }
+    }
+
     public static void main(String[] args) {
         Thread helloThread = new Thread(new HelloWorldRunnable());
         helloThread.start();
+
+        Thread evenThread = new Thread(new ParityRunnable(20, true));
+        Thread oddThread = new Thread(new ParityRunnable(20, false));
+        evenThread.start();
+        oddThread.start();
     }
 }
